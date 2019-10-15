e8c96fb500a96f6b668a9f46ea419c34109ed1cc
Shamel Lakin <christlife4u@gmail.com>
2019-10-15 16:45:02 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,11 @@
 require 'pry'
 
 class Artist
+  extend Memorable::ClassMethods
+  
   attr_accessor :name
   attr_reader :songs
-
+  
   @@artists = []
 
   def initialize
@@ -19,13 +21,13 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  # def self.reset_all
+  #   self.all.clear
+  # end
 
-  def self.count
-    self.all.count
-  end
+  # def self.count
+  #   self.all.count
+  # end
 
   def add_song(song)
     @songs << song
